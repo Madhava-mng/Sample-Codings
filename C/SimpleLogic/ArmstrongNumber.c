bf494560ce4e14a87e4e3b8731f9e38a12c0a15b
MαđЋανα <madhava29082000@gmail.com>
2022-10-15 09:24:30 +0530

    Update ArmstrongNumber.c

--- C/SimpleLogic/ArmstrongNumber.c
+++ C/SimpleLogic/ArmstrongNumber.c
@@ -9,7 +9,7 @@ int isArmstrong(int a){
   int l = len(a), t = 0, n = a;
   while(a){
     t +=  (int)pow(a%10, l);
-                a /= 10;
+    a /= 10;
   }
   return (t == n)? 1:0;
 }
@@ -17,6 +17,5 @@ int isArmstrong(int a){
 int main(void){
   int a;
   scanf("%d", &a);
-        printf("%d", len(a));
   printf("%d is%san armstrong number", a, isArmstrong(a)? " ":" not ");
 }
